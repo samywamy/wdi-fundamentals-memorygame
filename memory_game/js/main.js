6a402efb12d621f546c2737d6632a116c2ba40d9
samywamy <samywamy@Samanthas-MacBook-Pro.local>
2018-05-25 03:11:58 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+	};
+
+
+cardsInPlay.push('cards[cardId]');
+
+flipCard([0]);
+flipCard([2]);
